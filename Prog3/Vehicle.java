9fbf6a28fe5cc7a7410aabdc3d1dd942284c3183
Disha S Rao <dishasrao2006@gmail.com>
2025-09-04 21:48:05 +0530

    Create Vehicle.java

--- Prog3/Vehicle.java
+++ Prog3/Vehicle.java
@@ -1,1 +1,14 @@
+package transport;
+
+public abstract class Vehicle {
+    protected static String id;
+
+    public Vehicle(String id) {
+        this.id = id;
+        System.out.println("Vehicle() constructor called");
+    }
+
+    // abstract method signature
+    public abstract void deliver(String item, String place);
+}
 
