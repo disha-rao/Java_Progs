699109f2a56626b84ef89be7f2723fcd13b69c98
Disha S Rao <dishasrao2006@gmail.com>
2025-10-06 21:05:19 +0530

    Added Consumer.java

---
+++ Prog7/Consumer.java
@@ -0,0 +1,15 @@
+class Consumer extends Thread {
+    private MessageBoard board;
+
+    public Consumer(MessageBoard b) {
+        this.board = b;
+    }
+
+    @Override
+    public void run() {
+        String msg;
+        while (!(msg = board.get()).equals("DONE")) {
+            System.out.println("Consumed: " + msg);
+        }
+    }
+}
