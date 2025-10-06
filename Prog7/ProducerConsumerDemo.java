8a8434cc55017562c785ea7280a471b2328d5d40
Disha S Rao <dishasrao2006@gmail.com>
2025-10-06 21:07:54 +0530

    Added ProducerConsumerDemo.java

---
+++ Prog7/ProducerConsumerDemo.java
@@ -0,0 +1,12 @@
+public class ProducerConsumerDemo {
+    public static void main(String[] args) {
+        MessageBoard board = new MessageBoard();
+        Producer p = new Producer(board);
+        Consumer c = new Consumer(board);
+
+        p.start();
+        c.start();
+
+p.join();c.join();
+    }
+}
